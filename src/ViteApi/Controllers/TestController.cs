5c2d0023ff76bd6991e3397f63d590bf1fe3b1d9
naiq f <fangqiank@outlook.com>
2021-08-24 09:38:32 -0600

    init

---
+++ src/ViteApi/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViteApi.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Test result");
+        }
+    }
+}
---
+++ src/ViteApi/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ViteApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //spa service 
+            services.AddSpaStaticFiles(config =>
+            {
+                config.RootPath = "dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseCors(builder =>
+                //{
+                //    builder.AllowAnyHeader()
+                //        .AllowAnyMethod()
+                //        .AllowAnyOrigin();
+                //});
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            app.UseSpaStaticFiles();
+
+            app.UseSpa(spa =>
+            {
+                if(env.IsDevelopment())
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+            });
+        }
+    }
+}
